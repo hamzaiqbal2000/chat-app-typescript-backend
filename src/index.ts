4ab1f8854aec7932fba5012197812915b806b3b5
hamza12022skipq <hiqbal.bscs18seecs@seecs.edu.pk>
2023-05-07 00:10:00 +0500

    Integrating Socket.IO with nodejs application

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,16 @@ import express from 'express'
 import http from 'http'
 const app = express();
 const server = http.createServer(app);
+import { Server } from "socket.io"
+
+const io = new Server(server);
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile('D:/NodeJs projects/project to include in porfolio/chat-app - backend/src/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 server.listen(3000, () => {
